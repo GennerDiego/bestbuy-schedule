4b8d406ce9498b1e0e586b34f59f2279f2bea953
genner.almeida <genner.almeida@dextra-sw.com>
2018-11-26 21:20:02 -0200

    oi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 // const schedule = require('node-schedule')
 const { requisicao, send } = require('./fluxo.js')
 const moment = require('moment')
-const logger = require('heroku-logger')
+// const logger = require('heroku-logger')
 var port = process.env.PORT || 3000
 var app = express()
 
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.send(JSON.stringify({ Hello: 'World' }))
 })
 app.listen(port, function () {
-  logger.info(`Aplicação em pé!`)
+  console.log(`Aplicação em pé!`)
 })
 
 const run = async () => {
@@ -20,7 +20,7 @@ const run = async () => {
   if (x.overview.caAvailability.shipping.status === 'InStock') {
     await send()
   }
-  logger.info('Status: ' + x.overview.caAvailability.shipping.status + ' ' + moment().utcOffset(-2).format('YYYY-MM-DD HH:mm:ss'))
+  console.log('Status: ' + x.overview.caAvailability.shipping.status + ' ' + moment().format('YYYY-MM-DD HH:mm:ss'))
 }
 
 run()
