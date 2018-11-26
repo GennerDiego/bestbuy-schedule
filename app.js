9472c2b3d73b655ed9ff912f84726b09c11a447a
genner.almeida <genner.almeida@dextra-sw.com>
2018-11-26 15:16:54 -0200

    teste

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express')
+const schedule = require('node-schedule')
+const { requisicao, send } = require('./fluxo.js')
+const moment = require('moment')
 var port = process.env.PORT || 3000
 var app = express();
 
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}))
+schedule.scheduleJob('*/5 * * * *', async () => {
+    const x = await requisicao()
+    // SoldOutOnline
+    // InStock
+    // if (x.overview.caAvailability.shipping.status === 'InStock') {
+      await send()
+    // }
+    // console.log('Status: ' + x.overview.caAvailability.shipping.status + ' ' + moment().format('YYYY-MM-DD HH:mm:ss'))
 })
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
-});
---
+++ fluxo.js
@@ -0,0 +1,31 @@
+const request = require('request-promise')
+const nodemailer = require('nodemailer')
+
+const options = {
+  uri: 'https://bizhacks.bbycastatic.ca/mobile-si/si/v4/pdp/overview/13208347?lang=fr',
+  json: true
+}
+
+const requisicao = () => request.get(options)
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'gennerdiego@gmail.com',
+    pass: 'qtkjarvhfaaiwypu'
+  }
+})
+
+var mailOptions = {
+  from: 'gennerdiego@gmail.com',
+  to: 'gennerdiego@gmail.com; viviane.orge@gmail.com',
+  subject: 'JOGO EM ESTOQUE',
+  text: 'https://www.bestbuy.ca/en-ca/category/nintendo-switch-consoles/3457140.aspx?'
+}
+
+const send = () => transporter.sendMail(mailOptions)
+
+module.exports = {
+  requisicao,
+  send
+}
