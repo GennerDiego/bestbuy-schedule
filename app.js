6015590d2a1e30b046daac2cd8716ee5b4a2d301
genner.almeida <genner.almeida@dextra-sw.com>
2018-11-26 22:14:31 -0200

    teste

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-// var express = require('express')
-// const schedule = require('node-schedule')
-// const { requisicao, send } = require('./fluxo.js')
-// const moment = require('moment')
-// const logger = require('heroku-logger')
-// var port = process.env.PORT || 3000
-// var app = express()
+var express = require('express')
+const logger = require('heroku-logger')
+var port = process.env.PORT || 3000
+var app = express()
 
-// app.get('/', function (req, res) {
-//   res.send(JSON.stringify({ Hello: 'World' }))
-// })
-// app.listen(port, function () {
-//   logger.info(`Aplicação em pé!`)
-// })
-
-// const run = async () => {
-//   const x = await requisicao()
-//   // SoldOutOnline
-//   // InStock
-//   if (x.overview.caAvailability.shipping.status === 'InStock') {
-//     await send()
-//   }
-//   logger.info('Status: ' + x.overview.caAvailability.shipping.status + ' ' + moment().utcOffset(-2).format('YYYY-MM-DD HH:mm:ss'))
-// }
-
-// run()
-// schedule.scheduleJob('*/5 * * * *', async () => {
-//   const x = await requisicao()
-//   // SoldOutOnline
-//   // InStock
-//   if (x.overview.caAvailability.shipping.status === 'InStock') {
-//     await send()
-//   }
-//   logger.info('Status: ' + x.overview.caAvailability.shipping.status + ' ' + moment().format('YYYY-MM-DD HH:mm:ss'))
-// })
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({ Hello: 'World' }))
+})
+app.listen(port, function () {
+  logger.info(`Aplicação em pé!`)
+})
